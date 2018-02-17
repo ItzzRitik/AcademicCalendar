44fdff5175eca54ca777a20df9d5b3666b7a23ab
Ritik Srivastava <ritik.space@gmail.com>
2018-02-17 10:46:19 +0530

    Initial commit

---
+++ app/src/main/java/com/xtremecreations/academiccalendar/Login.java
@@ -0,0 +1,83 @@
+package com.xtremecreations.academiccalendar;
+
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.sdsmdg.kd.trianglify.models.Palette;
+import com.sdsmdg.kd.trianglify.views.TrianglifyView;
+
+public class Login extends AppCompatActivity {
+    TrianglifyView backG;
+    Animation anim;
+    ImageView ico_splash;
+    RelativeLayout login_div,logo_div,splash_cover;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        backG=findViewById(R.id.backG);
+        backG.setPalette(new Palette(getResources().getIntArray(R.array.theme)));
+
+        ico_splash=findViewById(R.id.ico_splash);
+        login_div=findViewById(R.id.login_div);
+        logo_div=findViewById(R.id.logo_div);
+        splash_cover=findViewById(R.id.splash_cover);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.logo_trans);
+                splash_cover.setVisibility(View.GONE);
+                ico_splash.setImageResource(R.mipmap.logo);
+                Animation anima = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.logo_reveal);
+                logo_div.setVisibility(View.VISIBLE);logo_div.startAnimation(anima);ico_splash.startAnimation(anim);
+                new Handler().postDelayed(new Runnable() {@Override public void run() {
+                    scaleY(login_div,48,300,new AccelerateDecelerateInterpolator());}},800);
+            }},1500);
+    }
+    public void scaleX(final View view,int x,int t, Interpolator interpolator)
+    {
+        ValueAnimator anim = ValueAnimator.ofInt(view.getMeasuredWidth(),(int)dptopx(x));anim.setInterpolator(interpolator);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+                layoutParams.width = (Integer) valueAnimator.getAnimatedValue();
+                view.setLayoutParams(layoutParams);
+            }
+        });
+        anim.setDuration(t);anim.start();
+    }
+    public void scaleY(final View view,int y,int t, Interpolator interpolator)
+    {
+        ValueAnimator anim = ValueAnimator.ofInt(view.getMeasuredHeight(),(int)dptopx(y));anim.setInterpolator(interpolator);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+                layoutParams.height = (Integer) valueAnimator.getAnimatedValue();
+                view.setLayoutParams(layoutParams);view.invalidate();
+            }
+        });
+        anim.setDuration(t);anim.start();
+    }
+    public float dptopx(float num)
+    {return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, num, getResources().getDisplayMetrics());}
+    public float pxtodp(float num)
+    {return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_PX, num, getResources().getDisplayMetrics());}
+}
